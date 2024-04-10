88984c082b6eaffd9ccd75449411797217a0ee47
Guerrero <xavor@hotmail.es>
2024-04-10 13:37:47 +0200

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import xheader from "@/components/xheader.vue"
+import navbar from "@/components/navbar.vue"
+import xfooter from "@/components/xfooter.vue"
+
+export default {
+  components: { navbar, xfooter, xheader },
+}
+
+</script>
+
+<template>
+  <div class="app-container ">
+    <xheader></xheader>
+    <div class="container">
+    <navbar></navbar>
+  </div>
+    <main>holaquetal
+    <!-- <router-view></router-view> -->
+    </main>
+    <xfooter></xfooter>
+
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+main {
+  flex: 1;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,46 @@
+import { createApp } from 'vue'
+import '@/scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import BadgeDirective from 'primevue/badgedirective'
+import Home from '@/components/Home.vue'
+import App from '@/App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {  faTrash, faCircleXmark, faMagnifyingGlass, faPenToSquare, faUserPlus } from '@fortawesome/free-solid-svg-icons' //iconos de muestra
+import { faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons' //iconos de muestra
+
+
+library.add( faInstagram, faXTwitter, faTrash, faCircleXmark, faMagnifyingGlass, faPenToSquare, faUserPlus)
+
+
+
+const NotFound = () => import('@/components/NotFound.vue')
+
+
+
+const routes = [
+    { path: '/', redirect: '/home'},
+    { path: '/home', component: Home, name: 'home' },
+    { path: '/:pathMatch(.*)', component: NotFound, name: 'notfound'}
+    
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+    })
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+app.use(pinia)
+app.use(PrimeVue)
+app.directive('badge', BadgeDirective)
+app.mount('#app')
