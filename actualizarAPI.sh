641f42993b1654a0ee6e5dbd0b374b0e47b09b50
Xavier Guerrero <144020944+GuerreroDIM46@users.noreply.github.com>
2024-04-22 01:04:56 +0200

    actualizando gitignore

--- actualizarAPI.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker stop gocourt-api
-docker rm gocourt-api
-git pull
-gradle build
-docker build -t gocourt-api .
-docker compose up -d
--- actualizarVUE.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker stop gocourt-vue
-docker rm gocourt-vue
-git pull
-cd frontend
-docker build -t gocourt-vue-img .
-docker compose up -d
