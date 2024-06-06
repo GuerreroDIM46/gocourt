8b02e1619ef559636db9d9c70ddc86598b02a7c8
Guerrero <xavor@hotmail.es>
2024-06-06 12:33:22 +0200

    expose search partido

--- src/main/java/es/mde/rest/PartidoController.java
+++ src/main/java/es/mde/rest/PartidoController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import es.mde.entidades.Partido;
 import es.mde.repositorios.PartidoDAO;
@@ -56,7 +57,7 @@ public class PartidoController {
     }
     
     @GetMapping("/partidos/search/partidosCoincidentes")
-    public ResponseEntity<List<Partido>> getCoincidentes(@RequestBody Partido partido) {
+    public ResponseEntity<List<Partido>> getCoincidentes(@RequestParam Partido partido) {
         List<Partido> partidos = new ArrayList<Partido>();
         partidos = partidoDAO.getPartidosByCampoYFechaHora(partido.getCampo().getId(), partido.getCuando());
         return new ResponseEntity<List<Partido>>(partidos, HttpStatus.OK);
