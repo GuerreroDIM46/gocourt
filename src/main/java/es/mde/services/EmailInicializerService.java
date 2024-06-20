32fe4f9a227051d3055982058d9125b7583aebee
Guerrero <xavor@hotmail.es>
2024-06-20 09:39:01 +0200

    Tokenizacion de la landing page completada. Fix #59, #60

--- src/main/java/es/mde/services/EmailInicializerService.java
+++ src/main/java/es/mde/services/EmailInicializerService.java
@@ -54,7 +54,6 @@ public class EmailInicializerService {
         String rechazarInvitacionUrl = direccionAPI + "puntuaciones/search/actualizarAsistencia?id=" + puntuacion1Id + "&aceptado=false";
         String aceptarIntercambioUrl = direccionAPI + "puntuaciones/search/actualizarCompartidoTelefono?id=" + puntuacion1Id + "&compartidoTelefono=true";
         String rechazarIntercambioUrl = direccionAPI + "puntuaciones/search/actualizarCompartidoTelefono?id=" + puntuacion1Id + "&compartidoTelefono=false";
-//        String introducirDetallesPartidoURL = direccionApp + "#/landingPage/" + partidoId;
         String introducirDetallesPartidoURL = direccionApp + "#/landingPage/" + partidoToken;
 
         Map<String, Object> variables = new HashMap<>();
